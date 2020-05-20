3d61987664e48c490c31e57fe498586b5b84ff0c
vermahim17 <vermahim17@gmail.com>
2020-05-20 20:27:35 +0530

    Varaiables example also shows type conversion now.

--- variables/Program.cs
+++ variables/Program.cs
@@ -7,9 +7,20 @@ namespace variables
         static void Main(string[] args)
         {
             byte number = 1;
-            int count = 10;
+            int count = 1000;
+
+            //casting or explicit conversion
+            number = (byte)count;
+
+            //'var' automatically identified the data type.
+            var num = "1234";
+            //non-compatible conversion
+            int new_num = Convert.ToInt32(num);
+
+            //cannot store more than 232 that is max limit
             Console.WriteLine(number);
             Console.WriteLine(count);
+            Console.WriteLine(new_num);
         }
     }
 }
