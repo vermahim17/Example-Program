b4897dcc65dd238d91e8dbcf99f1f9b235205f7a
vermahim17 <vermahim17@gmail.com>
2020-05-20 20:08:45 +0530

    simple variable program

---
+++ variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 1;
+            int count = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+        }
+    }
+}
