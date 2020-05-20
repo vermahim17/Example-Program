3f4dadc08daf32db33f910ea0828ee240e5d2964
vermahim17 <vermahim17@gmail.com>
2020-05-20 20:38:44 +0530

    Changing variable file to show type conversion
    This is my HelloWorld! program written in c-sharp.

--- variables/Program.cs
+++ variables/Program.cs
@@ -7,9 +7,21 @@ namespace variables
         static void Main(string[] args)
         {
             byte number = 1;
-            int count = 10;
+            int count = 1000;
+
+            //casting or explicit conversion
+            number = (byte)count;
+
+            //'var' automatically identified the data type.
+            var num = "1234";
+            //non-compatible conversion
+            int new_num = Convert.ToInt32(num);
+
+            Console.WriteLine("Examples of type conversion");
+            //cannot store more than 232 that is max limit
             Console.WriteLine(number);
             Console.WriteLine(count);
+            Console.WriteLine(new_num);
         }
     }
 }
